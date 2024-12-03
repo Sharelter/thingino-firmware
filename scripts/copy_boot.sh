0fd5f70d3d709a6d782f79b300d83bba414a5f1b
Sharelter <Sharelter@outlook.com>
2024-12-03 23:32:09 +0800

    update jooan

---
+++ scripts/copy_boot.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+#
+# Embedded Linux boot partition extractor
+#
+# This script extracts the boot partition from a firmware dump.
+# Tested on HiSilicon and Ingenic firmware dumps from NOR SPI flash chips.
+# Use at your own risk.
+#
+# 2023, Paul Philippov <paul@themactep.com>
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <stock firmware dump>"
+    exit 1
+fi
+
+echo_c() {
+    echo -e "\e[38;5;$1m$2\e[0m"
+}
+
+die() {
+    echo_c 124 "Error! $1"
+    exit 1
+}
+
+run() {
+    echo_c 94 "$1"
+    eval $1
+}
+
+say() {
+    echo_c 72 "\n$1"
+}
+
+full_binary_file="$1"
+
+bootcmd=$(strings "$full_binary_file" | grep -E "mtdparts=\w+_sfc:[0-9]" | tail -1)
+[ -z "$bootcmd" ] && die "Cannot determine boot command!"
+say "Boot command: $bootcmd"
+
+offset_bytes=0
+n=0
+
+say "looking for mtd partitions"
+mtdparts=$(echo $bootcmd | sed -E "s/(.*)(mtdparts=jz_sfc)/\\2/" | cut -d ' ' -f 1 | cut -d: -f2)
+[ -z "$mtdparts" ] && die "Cannot determine partitioning!"
+say "Partitioning: $mtdparts"
+
+for p in ${mtdparts//,/ }; do
+    p_size=$(echo $p | cut -d '(' -f 1)
+    p_name=$(echo $p | cut -d '(' -f 2 | cut -d ')' -f 1)
+
+    if [ "-" = "$p_size" ]; then
+        p_size_bytes=""
+    elif [ "0x" = "${p_size:0:2}" ]; then
+        ## convert hex values
+        p_size_bytes=$(($p_size))
+    else
+        p_size_value=$(echo $p_size | sed -E 's/[^0-9]//g')
+        p_size_unit=$(echo $p_size | sed -E 's/[0-9]+//')
+        if [ "k" = "${p_size_unit,,}" ]; then
+            p_size_bytes=$((p_size_value * 1024))
+        elif [ "m" = "${p_size_unit,,}" ]; then
+            p_size_bytes=$((p_size_value * 1024 * 1024))
+        else
+            p_size_bytes=$p_size_value
+        fi
+    fi
+
+    printf "%-14s\toffset: %8d\tsize: %8d\n" $p $offset_bytes $p_size_bytes
+    if [ "$p_name" = "boot" ]; then
+        echo_c 190 "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+        boot_file=boot.bin
+        boot_offset=$offset_bytes
+        boot_size=$p_size_bytes
+    fi
+
+    offset_bytes=$((offset_bytes + p_size_bytes))
+    n=$((n + 1))
+done
+
+[ -z "$boot_file" ] && die "Boot partition not found!"
+
+say "extract boot partition from full dump: $boot_size bytes at offset $boot_offset"
+run "dd if=$full_binary_file bs=1 skip=$boot_offset count=$boot_size of=$boot_file status=progress"
+
+say "Boot partition extracted to $boot_file"
+
+say "Done!"
+
+exit 0
